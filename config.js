f940a19d635d82b4910f61d03a0cbbb7f92c761c
tomotsugu-n <132063004+tomotsugu-n@users.noreply.github.com>
2026-07-23 22:38:17 +0900

    Update config.js
    add client ID

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Cloudで作成する「ウェブ アプリケーション」OAuthクライアントID。
 // 例: 1234567890-abcdef.apps.googleusercontent.com
-const GOOGLE_CLIENT_ID = "PASTE_WEB_OAUTH_CLIENT_ID_HERE.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "47789484837-j45us2a1q7mpp2oc6k3ktvdosr8vlknr.apps.googleusercontent.com";
